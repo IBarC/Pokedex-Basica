c94aa302d954cab9a5a8e1786f974cd84f1cf9e5
Irene <a_irene.barboteo.company@iespablopicasso.es>
2022-01-23 19:56:05 +0100

    Añadido archivo de sql

---
+++ Pokedex.sql
@@ -0,0 +1,4 @@
+create database Pokedex;
+use pokedex;
+
+select * from users;
--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -8,26 +8,26 @@ import java.sql.Statement;
 
 public class UsuarioDAO {
 
-	final String DB_URL;
-	final String USER;
-	final String PASS;
-	final String QUERRY;
+//	final String DB_URL;
+	//final String USER;
+	//final String PASS;
+	//final String QUERRY;
 	
-	public void Consulta() {
-		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS));
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(QUERRY){
-			while (rs.next()) {
+//	public void Consulta() {
+		//try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS));
+				//Statement stmt = conn.createStatement();
+				//ResultSet rs = stmt.executeQuery(QUERRY){
+			//while (rs.next()) {
 				//Display values
-				System.out.println("id: " + rs.getInt("id"));
-				System.out.println(", age: " + rs.getInt("age"));
-				System.out.println(", first: " + rs.getString("first"));
-				System.out.println(", last: " + rs.getString("last"));
-			}
+				//System.out.println("id: " + rs.getInt("id"));
+				//System.out.println(", age: " + rs.getInt("age"));
+//				System.out.println(", first: " + rs.getString("first"));
+	//			System.out.println(", last: " + rs.getString("last"));
+			//}
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+		//} catch (SQLException e) {
+			//e.printStackTrace();
+		//}
+	//}
 	
 }
